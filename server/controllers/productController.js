c852448f5ef42426440a6751fc0f77fea1cab469
Jacob Hall <halljaco@yahoo.com>
2020-04-29 14:30:49 -0600

    commit before pP

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,13 +1,34 @@
 module.exports = {
   createProduct: async (req, res) => {
     const db = req.app.get('db');
+    const {productName, price} = req.body;
+
+    const newProduct = await db.create_product([productName, price]);
+
+
+    if(newProduct) {
+      res.status(200).send(newProduct);
+    } else {
+      return res.status(500).send(`Something went wrong!`)
+    }
+
   },
+
+
+
   getProducts: async (req, res) => {
     const db = req.app.get('db');
   },
+
+
+
   updateProduct: async (req, res) => {
     const db = req.app.get('db');
   },
+
+
+
+
   deleteProduct: async (req, res) => {
     const db = req.app.get('db');
   },
